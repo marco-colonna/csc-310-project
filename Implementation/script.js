16d74afef908060cbd95200f95bf8421ae5eb0a6
Aaron-Prichard-umich <113215701+Aaron-Prichard-umich@users.noreply.github.com>
2022-11-27 20:23:44 -0500

    Update script.js

--- Implementation/script.js
+++ Implementation/script.js
@@ -1,12 +1,36 @@
 document.getElementById("balanceBtn").addEventListener("click", setBalanced);
 document.getElementById("perfBtn").addEventListener("click", setPerformance);
 document.getElementById("saverBtn").addEventListener("click", setSaver);
+document.getElementById("revert").addEventListener("click", setDefault);
 document.body.addEventListener("change", setCustom);
   var allScreen = document.getElementsByClassName("screen");
   var allDisk = document.getElementsByClassName("disk");
   var allMedia = document.getElementsByClassName("media");
   var allVideo = document.getElementsByClassName("video");
   var allPlaying = document.getElementsByClassName("playing");
+  var allPwrBtn = document.getElementsByClassName("pwrBtn");
+  var allSlpBtn = document.getElementsByClassName("slpBtn");
+	for(i = 0; i<allScreen.length; i++){
+		allScreen[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allDisk.length; i++){
+		allDisk[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allMedia.length; i++){
+		allMedia[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allVideo.length; i++){
+		allVideo[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allPlaying.length; i++){
+		allPlaying[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allPwrBtn.length; i++){
+		allPwrBtn[i].selectedIndex = "3";
+	}
+	for(i = 0; i<allSlpBtn.length; i++){
+		allSlpBtn[i].selectedIndex = "1";
+	}
 function setBalanced() {
   document.getElementById("plan").innerHTML = "Balanced";
 	for(i = 0; i<allScreen.length; i++){
@@ -23,6 +47,12 @@ function setBalanced() {
 	}
 	for(i = 0; i<allPlaying.length; i++){
 		allPlaying[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allPwrBtn.length; i++){
+		allPwrBtn[i].selectedIndex = "3";
+	}
+	for(i = 0; i<allSlpBtn.length; i++){
+		allSlpBtn[i].selectedIndex = "1";
 	}
 }
 
@@ -43,6 +73,12 @@ function setPerformance() {
 	for(i = 0; i<allPlaying.length; i++){
 		allPlaying[i].selectedIndex = "0";
 	}
+	for(i = 0; i<allPwrBtn.length; i++){
+		allPwrBtn[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allSlpBtn.length; i++){
+		allSlpBtn[i].selectedIndex = "4";
+	}
 }
 
 function setSaver() {
@@ -62,8 +98,41 @@ function setSaver() {
 	for(i = 0; i<allPlaying.length; i++){
 		allPlaying[i].selectedIndex = "2";
 	}
+	for(i = 0; i<allPwrBtn.length; i++){
+		allPwrBtn[i].selectedIndex = "3";
+	}
+	for(i = 0; i<allSlpBtn.length; i++){
+		allSlpBtn[i].selectedIndex = "2";
+	}
 }
 
 function setCustom() {
-  document.getElementById("plan").innerHTML = "Custom";
+	if(event.target.tagName == "SELECT"){
+		document.getElementById("plan").innerHTML = "Custom";
+	}
+}
+
+function setDefault() {
+		document.getElementById("plan").innerHTML = "Default";
+	for(i = 0; i<allScreen.length; i++){
+		allScreen[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allDisk.length; i++){
+		allDisk[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allMedia.length; i++){
+		allMedia[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allVideo.length; i++){
+		allVideo[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allPlaying.length; i++){
+		allPlaying[i].selectedIndex = "1";
+	}
+	for(i = 0; i<allPwrBtn.length; i++){
+		allPwrBtn[i].selectedIndex = "3";
+	}
+	for(i = 0; i<allSlpBtn.length; i++){
+		allSlpBtn[i].selectedIndex = "1";
+	}
 }
